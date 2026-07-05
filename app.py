c68d03732b5644fd29e40c3aba3dfc89cab9a1e0
zinhaninunes <pedroexpertise@gmail.com>
2026-07-05 14:14:29 -0300

    novos arquivos adicionados: config.py, database.py, requirements.txt e modificações em app.py, templates/login.html e templates/register.html.

--- app.py
+++ app.py
@@ -1,17 +1,84 @@
+from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
+import hashlib
 
-conexao = sqlite3.connect("usuarios.db")
-cursor = conexao.cursor()
+app = Flask(__name__)
 
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS usuarios(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    nome TEXT NOT NULL,
-    email TEXT UNIQUE NOT NULL,
-    senha TEXT NOT NULL,
-    saldo REAL DEFAULT 0
-)
-""")
 
-conexao.commit()
-conexao.close()
+# Conectar ao banco
+def conectar():
+    return sqlite3.connect("usuarios.db")
+
+
+# Página inicial
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+# Página de login
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+# Página de cadastro
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+
+# Receber cadastro
+@app.route("/cadastrar", methods=["POST"])
+def cadastrar():
+
+    nome = request.form["usuario"]
+    email = request.form["email"]
+    senha = hashlib.sha256(
+        request.form["senha"].encode()
+    ).hexdigest()
+
+    con = conectar()
+    cursor = con.cursor()
+
+    cursor.execute("""
+        INSERT INTO usuarios
+        (nome,email,senha,saldo)
+        VALUES(?,?,?,?)
+    """, (nome, email, senha, 0))
+
+    con.commit()
+    con.close()
+
+    return redirect(url_for("login"))
+
+
+# Receber login
+@app.route("/entrar", methods=["POST"])
+def entrar():
+
+    email = request.form["email"]
+    senha = hashlib.sha256(
+        request.form["senha"].encode()
+    ).hexdigest()
+
+    con = conectar()
+    cursor = con.cursor()
+
+    cursor.execute("""
+        SELECT * FROM usuarios
+        WHERE email=? AND senha=?
+    """, (email, senha))
+
+    usuario = cursor.fetchone()
+
+    con.close()
+
+    if usuario:
+        return "Login realizado!"
+    else:
+        return "Email ou senha incorretos."
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
