d98ee83227f187838ab801997107c72ae271b43f
zinhaninunes <pedroexpertise@gmail.com>
2026-07-05 21:01:14 -0300

    coreções pequenas no código, ajustes de layout e melhorias na interface do usuário.

--- app.py
+++ app.py
@@ -1,19 +1,42 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash, session
+from pathlib import Path
 import sqlite3
 import hashlib
+import secrets
 
 app = Flask(__name__)
-
+BASE_DIR = Path(__file__).resolve().parent
+DB_PATH = BASE_DIR / "usuarios.db"
+app.secret_key = secrets.token_hex(32)
 
 # Conectar ao banco
 def conectar():
-    return sqlite3.connect("usuarios.db")
+    return sqlite3.connect(DB_PATH)
 
 
 # Página inicial
 @app.route("/")
 def home():
-    return render_template("home.html")
+
+    if "usuario_id" not in session:
+        return render_template("home.html")
+
+    con = conectar()
+    cursor = con.cursor()
+
+    cursor.execute(
+        "SELECT saldo FROM usuarios WHERE id=?",
+        (session["usuario_id"],)
+    )
+
+    saldo = cursor.fetchone()[0]
+
+    con.close()
+
+    return render_template(
+        "home.html",
+        saldo=saldo
+    )
 
 
 # Página de login
@@ -21,6 +44,9 @@ def home():
 def login():
     return render_template("login.html")
 
+@app.route("/games/tigrinho")
+def tigrinho():
+    return render_template("games/Tigrinho.html")
 
 # Página de cadastro
 @app.route("/register")
@@ -34,22 +60,46 @@ def cadastrar():
 
     nome = request.form["usuario"]
     email = request.form["email"]
-    senha = hashlib.sha256(
-        request.form["senha"].encode()
-    ).hexdigest()
 
     con = conectar()
     cursor = con.cursor()
 
-    cursor.execute("""
-        INSERT INTO usuarios
-        (nome,email,senha,saldo)
-        VALUES(?,?,?,?)
-    """, (nome, email, senha, 0))
+    cursor.execute(
+        "SELECT 1 FROM usuarios WHERE email = ?",
+        (email,)
+    )
 
-    con.commit()
-    con.close()
+    if cursor.fetchone():
+        con.close()
+        flash("email já cadastrado.")
+        return redirect(url_for("register"))
+    
+    senha = request.form["senha"]
+    confirmar = request.form["confirmar_senha"]
 
+    if senha != confirmar:
+        flash("senhas não coincidem.")
+        return redirect(url_for("register"))
+
+    senha = hashlib.sha256(senha.encode()).hexdigest()
+
+    try:
+        cursor.execute("""
+            INSERT INTO usuarios
+            (nome, email, senha, saldo)
+            VALUES (?, ?, ?, ?)
+        """, (nome, email, senha, 0))
+
+        con.commit()
+
+    except sqlite3.IntegrityError:
+        flash("algo deu errado, tente novamente.")
+        return redirect(url_for("register"))
+
+    finally:
+        con.close()
+
+    flash("Conta criada com sucesso!")
     return redirect(url_for("login"))
 
 
@@ -75,9 +125,19 @@ def entrar():
     con.close()
 
     if usuario:
-        return "Login realizado!"
+        session["usuario_id"] = usuario[0]
+        session["nome"] = usuario[1]
+        session["saldo"] = usuario[4]
+
+        return redirect(url_for("home"))
     else:
-        return "Email ou senha incorretos."
+        flash("Email ou senha incorretos.")
+        return redirect(url_for("login"))
+    
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect(url_for("login"))
 
 
 if __name__ == "__main__":
